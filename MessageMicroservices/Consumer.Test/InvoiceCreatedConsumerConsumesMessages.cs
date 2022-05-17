cc6b8448256f4fc4055d29728c58ea09a33a3e65
John Mason <john.mason@pebblepad.co.uk>
2022-05-17 09:46:04 +0100

    Last section of chapter 7 - Testing messaging microservices

---
+++ MessageMicroservices/Consumer.Test/InvoiceCreatedConsumerConsumesMessages.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using MassTransit;
+using MassTransit.Testing;
+using MessageContracts;
+using PaymentMicroservice;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Consumer.Test {
+    public class InvoiceCreatedConsumerConsumesMessages {
+        [Fact]
+        public async Task VerifyInvoiceCreatedMessageConsumed() {
+            var harness = new InMemoryTestHarness();
+
+            var consumerHarness = harness.Consumer<InvoiceCreatedConsumer>();
+            await harness.Start();
+
+            try {
+                await harness.Bus.Publish<IInvoiceCreated>(
+                    new { 
+                        InvoiceCreated = InVar.Id 
+                    }
+                );
+
+                Assert.True(await harness.Consumed.Any<IInvoiceCreated>());
+
+                Assert.True(await consumerHarness.Consumed.Any<IInvoiceCreated>());
+
+                harness.Published.Select<IInvoiceCreated>().Count().Should().Be(1);
+            } finally {
+                await harness.Stop();
+            }
+        }
+    }
+}
---
+++ MessageMicroservices/Producer.Test/InvoiceToCreateCommandConsumesMessages.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using InvoiceMicroservice;
+using MassTransit.Testing;
+using MessageContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Producer.Test {
+    public class InvoiceToCreateCommandConsumesMessages {
+        [Fact]
+        public async Task VerifyInvoiceToCreateCommandConsumed() {
+            var harness = new InMemoryTestHarness();
+            var consumerHarness = harness.Consumer<EventConsumer>();
+
+            await harness.Start();
+
+            try {
+                await harness.InputQueueSendEndpoint.Send<IInvoiceToCreate>(
+                    new {
+                        CustomerNumber = 19282,
+                        InvoiceItems = new List<InvoiceItems> {
+                            new InvoiceItems {
+                                Price = Math.Round(1020.99),
+                                ActualMileage = 40,
+                                BaseRate = 12.50,
+                                IsHazardousMaterial = false,
+                                IsOversized = true,
+                                IsRefrigerated = false
+                            }
+                        }
+                    });
+
+                Assert.True(await harness.Consumed.Any<IInvoiceToCreate>());
+
+                Assert.True(await consumerHarness.Consumed.Any<IInvoiceToCreate>());
+
+                harness.Published.Select<IInvoiceCreated>().Count().Should().Be(1);
+            } finally {
+                await harness.Stop();
+            }
+        }
+    }
+}
